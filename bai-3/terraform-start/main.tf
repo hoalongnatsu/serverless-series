52b0adde8dd0f3aefb6e868210494dcca799bed4
hoalongnatsu <hoalongnatsu@gmail.com>
2021-12-16 16:57:11 +0700

    add code bai-3

--- bai-3/terraform-start/main.tf
+++ bai-3/terraform-start/main.tf
@@ -35,6 +35,10 @@ resource "aws_api_gateway_method" "books" {
 }
 
 resource "aws_api_gateway_integration" "books" {
+  depends_on = [
+    aws_lambda_function.function
+  ]
+
   for_each    = toset(["list", "create", "update", "delete"])
   rest_api_id = aws_api_gateway_rest_api.books.id
   resource_id = aws_api_gateway_resource.books.id

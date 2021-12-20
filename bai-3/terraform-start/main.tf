4948ef0fd04e8f3ca4fa584c9f0d44048d37e14c
hoalongnatsu <hoalongnatsu@gmail.com>
2021-12-20 23:14:01 +0700

    update bai-3 terraform

--- bai-3/terraform-start/main.tf
+++ bai-3/terraform-start/main.tf
@@ -35,18 +35,14 @@ resource "aws_api_gateway_method" "books" {
 }
 
 resource "aws_api_gateway_integration" "books" {
-  depends_on = [
-    aws_lambda_function.function
-  ]
-
-  for_each    = toset(["list", "create", "update", "delete"])
+  for_each    = { for k, v in aws_lambda_function.function : k => v if v.function_name != "books_get" }
   rest_api_id = aws_api_gateway_rest_api.books.id
   resource_id = aws_api_gateway_resource.books.id
   http_method = aws_api_gateway_method.books.http_method
 
   integration_http_method = "POST"
   type                    = "AWS_PROXY"
-  uri                     = aws_lambda_function.function[each.value].invoke_arn
+  uri                     = each.value.invoke_arn
 }
 
 resource "aws_api_gateway_resource" "books_proxy" {
@@ -83,10 +79,10 @@ resource "aws_api_gateway_deployment" "deployment" {
 }
 
 resource "aws_lambda_permission" "apigw" {
-  for_each      = toset(local.names)
+  for_each      = aws_lambda_function.function
   statement_id  = "AllowAPIGatewayInvoke"
   action        = "lambda:InvokeFunction"
-  function_name = aws_lambda_function.function[each.value].function_name
+  function_name = each.value.function_name
   principal     = "apigateway.amazonaws.com"
 
   source_arn = "${aws_api_gateway_rest_api.books.execution_arn}/*/*"

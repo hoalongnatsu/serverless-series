8c658d7e681d0f2e369ec976613d05b3a6498131
hoalongnatsu <hoalongnatsu@gmail.com>
2021-12-20 23:14:17 +0700

    add bai-4 terraform

---
+++ bai-4/terraform-start/main.tf
@@ -0,0 +1,93 @@
+provider "aws" {
+  region = "us-west-2"
+}
+
+locals {
+  names = ["list", "get", "create", "delete"]
+}
+
+resource "aws_lambda_function" "function" {
+  for_each      = toset(local.names)
+  function_name = "books_${each.value}"
+  role          = aws_iam_role.lambda_role.arn
+  handler       = "main"
+  runtime       = "go1.x"
+
+  filename         = "source/${each.value}.zip"
+  source_code_hash = filebase64sha256("source/${each.value}.zip")
+}
+
+resource "aws_api_gateway_rest_api" "books" {
+  name = "books-api"
+}
+
+resource "aws_api_gateway_resource" "books" {
+  rest_api_id = aws_api_gateway_rest_api.books.id
+  parent_id   = aws_api_gateway_rest_api.books.root_resource_id
+  path_part   = "books"
+}
+
+resource "aws_api_gateway_method" "books" {
+  rest_api_id   = aws_api_gateway_rest_api.books.id
+  resource_id   = aws_api_gateway_resource.books.id
+  http_method   = "ANY"
+  authorization = "NONE"
+}
+
+resource "aws_api_gateway_integration" "books" {
+  for_each    = { for k, v in aws_lambda_function.function : k => v if v.function_name != "books_get" }
+  rest_api_id = aws_api_gateway_rest_api.books.id
+  resource_id = aws_api_gateway_resource.books.id
+  http_method = aws_api_gateway_method.books.http_method
+
+  integration_http_method = "POST"
+  type                    = "AWS_PROXY"
+  uri                     = each.value.invoke_arn
+}
+
+resource "aws_api_gateway_resource" "books_proxy" {
+  rest_api_id = aws_api_gateway_rest_api.books.id
+  parent_id   = aws_api_gateway_resource.books.id
+  path_part   = "{id}"
+}
+
+resource "aws_api_gateway_method" "books_proxy" {
+  rest_api_id   = aws_api_gateway_rest_api.books.id
+  resource_id   = aws_api_gateway_resource.books_proxy.id
+  http_method   = "GET"
+  authorization = "NONE"
+}
+
+resource "aws_api_gateway_integration" "books_proxy" {
+  rest_api_id = aws_api_gateway_rest_api.books.id
+  resource_id = aws_api_gateway_resource.books_proxy.id
+  http_method = aws_api_gateway_method.books_proxy.http_method
+
+  integration_http_method = "POST"
+  type                    = "AWS_PROXY"
+  uri                     = aws_lambda_function.function["get"].invoke_arn
+}
+
+resource "aws_api_gateway_deployment" "deployment" {
+  depends_on = [
+    aws_api_gateway_integration.books,
+    aws_api_gateway_integration.books_proxy,
+  ]
+
+  rest_api_id = aws_api_gateway_rest_api.books.id
+  stage_name  = "staging"
+}
+
+resource "aws_lambda_permission" "apigw" {
+  for_each      = aws_lambda_function.function
+  statement_id  = "AllowAPIGatewayInvoke"
+  action        = "lambda:InvokeFunction"
+  function_name = each.value.function_name
+  principal     = "apigateway.amazonaws.com"
+
+  source_arn = "${aws_api_gateway_rest_api.books.execution_arn}/*/*"
+}
+
+output "base_url" {
+  value = aws_api_gateway_deployment.deployment.invoke_url
+}
